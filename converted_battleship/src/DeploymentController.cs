9b77ff945efb83b618dc8be1e91c0c6c26c88438
Ajtn <alexistn@hotmail.com>
2015-09-23 17:57:26 +1000

    conversion error
    changed conversion to i = ((int) sn) - 1;

--- converted_battleship/src/DeploymentController.cs
+++ converted_battleship/src/DeploymentController.cs
@@ -144,7 +144,7 @@ static class DeploymentController
 		//DrawShips
 		foreach (ShipName sn in Enum.GetValues(typeof(ShipName))) {
 			int i = 0;
-			i = Conversion.Int(sn) - 1;
+			i = ((int) sn) - 1;
 			if (i >= 0) {
 				if (sn == _selectedShip) {
 					SwinGame.DrawBitmap(GameImage("SelectedShip"), SHIPS_LEFT, SHIPS_TOP + i * SHIPS_HEIGHT);
